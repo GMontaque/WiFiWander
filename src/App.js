1871fbb3d4adb1e4a2f4d4fa6d14a4473dcaaf73
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-20 23:21:08 +0000

    feat: install and create routing feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import Homepage from './pages/Homepage';
+import { Route, Routes } from "react-router-dom";
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignUp';
+import NotFound from './components/NotFound';
+import "./api/axiosDefaults";
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          add new text
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Container fluid>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignOut />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            add new text
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Footer />
+      </Container>
     </div>
   );
 }
