bd03d7962d90c0faaa00f25dfaea45873c4a7810
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-20 23:18:35 +0000

    feat: create sign in, out and footer pages

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Footer() {
+    return (
+        <>
+            <Container fluid>
+                <Row>
+                    <footer>
+                        <Row>
+                            <Col>company name and logo</Col>
+                            <Col>nav bar links</Col>
+                        </Row>
+                        <Row>
+                            <Col>social meida links</Col>
+                            <Col>company rights info</Col>
+                        </Row>
+                    </footer>
+                </Row>
+            </Container>
+        </>
+
+    )
+}
+
+export default Footer
