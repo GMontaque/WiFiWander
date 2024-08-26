14cb469e6d76c23d69a2b582311ba2fc3eacf7e6
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-26 07:51:10 +0000

    feat: install react router dom and create routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar";
 import Footer from './components/Footer';
@@ -8,38 +7,44 @@ import { Route, Routes } from "react-router-dom";
 import SignIn from './pages/SignIn';
 import SignOut from './pages/SignUp';
 import NotFound from './components/NotFound';
-import "./api/axiosDefaults";
-
+import Profile from './pages/Profile';
+import Hero from './components/Hero';
+import Country from './pages/Country';
+import City from './pages/City';
+import WifiLocationsList from './pages/WifiLocationsList';
+import WifiLocationsCreation from './pages/WifiLocationsCreation';
+import About from './pages/About';
+import WifiLocationsPage from './pages/WifiLocationsPage';
+import Password from './pages/Password';
 
 
 function App() {
+
+
   return (
+
     <div className="App">
       <NavBar />
+      <Hero />
       <Container fluid>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignOut />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/continents/:continentName" element={<Country />} />
+          <Route path="/continents/:continentName/:countryName" element={<City />} />
+          <Route path="/newlocation" element={<WifiLocationsCreation />} />
+          <Route path="/continents/:continentName/:countryName/:cityName" element={<WifiLocationsList />} />
+          <Route path="/wifipage" element={<WifiLocationsPage />} />
+          <Route path="/password" element={<Password />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            add new text
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </Container>
       <Footer />
     </div>
+
   );
 }
 
