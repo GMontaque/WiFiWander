495c46e387a011189572b984e7362899bd812da4
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-26 10:41:23 +0000

    feat: create component for amenities icon key

---
+++ src/components/AmenitiesKey.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AmenitiesKey = () => {
+  return (
+    <>
+    <div>AmenitiesKey</div>
+    <p>icon <strong>title of icon</strong></p>
+    </>
+    
+  )
+}
+
+export default AmenitiesKey
