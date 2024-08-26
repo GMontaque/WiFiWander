c0a55c850f23a4fce06b68b1afb8dc8295fbbd5c
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-26 09:56:16 +0000

    feat: cerate hero image for homepage and content pages

---
+++ src/components/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
+import heroImageHome from '../assets/3949076.jpg';
+
+
+function Hero() {
+    const location = useLocation();
+
+    const HeroImage = () => {
+        if (location.pathname === '/') {
+            // return heroImageHome
+            return <Image src={heroImageHome} className="img-fluid" alt="top" style={{ width: '100%', height: '50rem' }} />
+        } else {
+            // return heroImageHome
+            return <Image src={heroImageHome} className="img-fluid" alt="top" style={{ width: '100%', height: '20rem' }} />
+        }
+    };
+
+    return (
+        <>
+            {HeroImage()}
+        </>
+    );
+}
+
+export default Hero;
