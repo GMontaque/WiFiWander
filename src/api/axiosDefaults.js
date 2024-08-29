b9e39320b0cb9058b5310f76965f8a00c5069fd2
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-29 07:22:28 +0000

    bug: correct error with baseURL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://wifi-wander-api-835560a3f6c2.herokuapp.com/";
+axios.defaults.baseURL = "https://wifi-wander-api-835560a3f6c2.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
