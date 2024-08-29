075a739286c2b566b1242e5ee110e9a57f1ee8a9
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-29 07:21:58 +0000

    feat: add continent images on homepage

--- src/pages/Continents.js
+++ src/pages/Continents.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import heroImage from '../assets/headerImage.webp';
+import africaImage from '../assets/africa.png';
+import asiaImage from '../assets/asia.png';
+import europeImage from '../assets/europe.png';
+import northAmericaImage from '../assets/north_america.png';
+import southAmericaImage from '../assets/south_america.png';
+import australiaImage from '../assets/oceania.png';
 
 const continents = [
-  { name: 'Africa', path: '/continents/africa' },
-  { name: 'Asia', path: '/continents/asia' },
-  { name: 'Europe', path: '/continents/europe' },
-  { name: 'North America', path: '/continents/north_america' },
-  { name: 'South America', path: '/continents/south_america' },
-  { name: 'Australia', path: '/continents/australia' },
+  { name: 'Africa', path: '/continents/africa', image: africaImage },
+  { name: 'Asia', path: '/continents/asia', image: asiaImage },
+  { name: 'Europe', path: '/continents/europe', image: europeImage },
+  { name: 'North America', path: '/continents/north_america', image: northAmericaImage },
+  { name: 'South America', path: '/continents/south_america', image: southAmericaImage },
+  { name: 'Australia', path: '/continents/australia', image: australiaImage },
 ];
 
 const Continents = () => {
@@ -17,7 +22,7 @@ const Continents = () => {
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
       {continents.map((continent) => (
         <Card key={continent.name} className="mb-4" style={{ width: '30rem' }}>
-          <Card.Img variant="top" src={heroImage} />
+          <Card.Img variant="top" src={continent.image} />
           <Card.Body>
             <Card.Title>{continent.name}</Card.Title>
             <Card.Text>
