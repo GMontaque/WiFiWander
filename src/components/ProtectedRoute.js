e634bcd3c93bec90e31c3721dcd2b30e6b11d0d0
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-31 06:51:41 +0000

    feat: create protected route component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useCurrentUser } from '../components/CurrentUserContext';
+import Loader from './Loader'
+
+const ProtectedRoute = ({ children }) => {
+    const currentUser = useCurrentUser();
+
+    if (currentUser === null) {
+        return <Loader />
+    }
+
+    if (!currentUser) {
+        return <Navigate to="/signin" />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
