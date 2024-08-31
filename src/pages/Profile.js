f59c6e542f0ed6ae474d1dcf2291169bd13bad76
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-31 06:34:38 +0000

    feat: create user profile page with favourites and created tabs

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Image, Row, Tab, Tabs } from 'react-bootstrap';
+import CreatedTab from '../components/CreatedTab';
+import FavouritesTab from '../components/FavouritesTab';
+import { useCurrentUser } from '../components/CurrentUserContext';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const currentUser = useCurrentUser();
 
-export default Profile
+  return (
+    <>
+      <Row>
+        <Image
+          src="https://via.placeholder.com/150"
+          style={{ width: '10rem', height: '10rem', display: 'block', margin: '0 auto' }}
+          roundedCircle
+        />
+        <h1>{currentUser?.username}</h1>
+      </Row>
+      <Row>
+        <Tabs defaultActiveKey="created" id="fill-tab-example" className="mb-3" fill>
+          <Tab eventKey="created" title="Created">
+            <CreatedTab username={currentUser?.username} />
+          </Tab>
+          <Tab eventKey="favourites" title="Favourites">
+            <FavouritesTab />
+          </Tab>
+        </Tabs>
+      </Row>
+    </>
+  );
+};
+
+export default Profile;
