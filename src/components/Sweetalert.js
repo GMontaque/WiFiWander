64d3bf1e5dd8d6f6c8d679cdd256f5f937157e07
Giulio Montaque <giuliom@hotmail.co.uk>
2024-08-31 09:25:36 +0000

    feat: create moduler alert for all pop up messages using sweetalert

---
+++ src/components/Sweetalert.js
@@ -0,0 +1,18 @@
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
+
+const showAlert = (actionTitle, actionText, actionIcon) => {
+    console.log(actionTitle, actionText, actionIcon)
+    MySwal.fire({
+        title: actionTitle,
+        text: actionText,
+        icon: actionIcon,
+        showConfirmButton: false,
+        timer: 2000,
+        showCloseButton: true,
+    });
+};
+
+export default showAlert;
