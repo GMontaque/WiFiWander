3743eb82349b1240085ed099fdb6ec9fa5f759b9
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-02 08:17:24 +0000

    feat: fetch real wifi location data from database and display results

--- src/pages/WifiLocationsPage.js
+++ src/pages/WifiLocationsPage.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { axiosReq } from "../api/axiosDefaults";
 import { Image, Row, Button } from 'react-bootstrap';
 import AmenitiesKey from '../components/AmenitiesKey';
 import { useCurrentUser } from '../components/CurrentUserContext';
 
+import { useParams } from 'react-router-dom';
 
 const WifiLocationsPage = () => {
-  const wifiLocation = {
-    name: "test",
-    star_rating: 2,
-    image: null,
-    description: "afrsadfsdfs",
-    amenities: "wifi"
-  }
+  const { id } = useParams();
+  const [wifiLocation, setWifiLocation] = useState(null);
+  const [error, setError] = useState(null);
   const currentUser = useCurrentUser();
 
+  useEffect(() => {
+    const fetchWifiLocation = async () => {
+      try {
+        const response = await axiosReq.get(
+          `/wifi_locations/${id}`
+        );
+        setWifiLocation(response.data);
+      } catch (err) {
+        setError('Failed to fetch WiFi location data');
+        console.error(err);
+      }
 
+    };
+
+    fetchWifiLocation();
+  }, [id]);
+
+
+  console.log(error);
   return (
     <>
       <Row>
         <h1>{wifiLocation.name}</h1>
         <span>
           {currentUser && (
-            <Button >Add to Favorites</Button>
+            <Button>Add to Favorites</Button>
           )}
         </span>
         <div>{wifiLocation.star_rating || 'No rating available'}</div>
