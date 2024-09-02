9547440352f50ff79af49a045d71856e0ee34b17
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-02 08:27:56 +0000

    feat: create form to add comment to wifi location

--- src/components/CreateComment.js
+++ src/components/CreateComment.js
@@ -1,46 +1,61 @@
-import React from 'react'
-import { Form, Button } from 'react-bootstrap'
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+const CreateComment = ({ username, commentToEdit }) => {
 
-const WifiLocationsCreation = () => {
 
-    return (
-        <>
-            <h1>Wifi Location Creation</h1>
-            <Form>
-                {/* user name */}
-                <Form.Group className="mb-3" controlId="Name">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Please enter your name" />
-                </Form.Group>
-                {/* location description */}
-                <Form.Group className="mb-3" controlId="description">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control as="textarea" rows={3} placeholder="Please describe the wifi location" />
-                </Form.Group>
-                {/* location star rating */}
-                <Form.Group className="mb-3" controlId="starRating">
-                    <Form.Label>Star Rating</Form.Label>
-                    {['radio'].map((type) => (
-                        <div key={`inline-${type}`} className="mb-3">
-                            {[1, 2, 3, 4, 5].map((int, index) => (
-                                <Form.Check
-                                    inline
-                                    label={index + 1}
-                                    name="group1"
-                                    type={type}
-                                    id={`inline-${type}-${index + 1}`}
-                                    key={index}
-                                />
-                            ))}
-                        </div>
-                    ))}
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>{commentToEdit ? 'Update Comment' : 'Create Comment'}</h1>
+      <Form >
+        <Form.Group className="mb-3" controlId="comment_text">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="input"
+            name="username"
+            value={username.username}
+            readOnly
+          />
 
-export default WifiLocationsCreation
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="comment_text">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Please describe the wifi location"
+            name="comment_text"
+
+
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="star_rating">
+          <Form.Label>Star Rating</Form.Label>
+          {[1, 2, 3, 4, 5].map((int) => (
+            <Form.Check
+              inline
+              label={int}
+              name="star_rating"
+              type="radio"
+              id={`inline-radio-${int}`}
+              key={int}
+              value={int}
+
+            />
+          ))}
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          {commentToEdit ? 'Update' : 'Submit'}
+        </Button>
+        {commentToEdit && (
+          <Button variant="secondary" className="ms-2">
+            Cancel
+          </Button>
+        )}
+      </Form>
+    </>
+  );
+};
+
+export default CreateComment;
