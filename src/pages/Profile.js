020dec9394e87f7ed49ee53ddfc81ab7da20ebbe
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-11 07:32:52 +0000

    Style: add styling to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -12,17 +12,17 @@ const Profile = () => {
       <Row>
         <Image
           src="https://via.placeholder.com/150"
-          style={{ width: '10rem', height: '10rem', display: 'block', margin: '0 auto' }}
+          className='profile-img'
           roundedCircle
         />
-        <h1>{currentUser?.username}</h1>
+        <h1 className='text-center capitalize wificreation-title' >{currentUser?.username}</h1>
       </Row>
-      <Row>
-        <Tabs defaultActiveKey="created" id="fill-tab-example" className="mb-3" fill>
-          <Tab eventKey="created" title="Created">
+      <Row className='profile-row'>
+        <Tabs defaultActiveKey="created" className="mb-3" fill>
+          <Tab eventKey="created" title="Created" tabClassName="tab-border">
             <CreatedTab username={currentUser?.username} />
           </Tab>
-          <Tab eventKey="favourites" title="Favourites">
+          <Tab eventKey="favourites" title="Favourites" tabClassName="tab-border">
             <FavouritesTab />
           </Tab>
         </Tabs>
