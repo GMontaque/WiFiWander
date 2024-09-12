ba06e8f8c70d91c4a144349ae5cff8ae0061f933
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-12 07:59:51 +0000

    style: add styling to 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Image, Row } from 'react-bootstrap'
+import teamImg from '../assets/team.jpg'
 
 const NotFound = () => {
   return (
     <Container>
-      <Row>
-        <h1>Error 404, page not found</h1>
+      <Row className='justify-content-center margin-bottom-7'>
+        <h1 className='pageTitle mt-5 mb-5'>Error 404, page not found</h1>
+        <Image src={teamImg} className='img-fluid mt-5 mb-5' />
       </Row>
     </Container>
   )
