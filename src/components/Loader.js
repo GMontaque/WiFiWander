e720b14712652c34b448f5da5fa9c537f92763d4
Giulio Montaque <giuliom@hotmail.co.uk>
2024-09-15 07:42:14 +0000

    style: add background to loader icon

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import { Spinner } from 'react-bootstrap'
 
 const Loader = () => {
   return (
-    <div className='loaderStyle'>
+    <div className='loaderStyle back-colour'>
       <Spinner animation="border" role="status" className='spin-icon'>
         <span className="visually-hidden">Loading...</span>
       </Spinner>
